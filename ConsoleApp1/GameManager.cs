fe2ea8389edb7dacca1a8e51b8515981ca302e00
JinyoungWoo <woo96923@gmail.com>
2020-10-27 22:05:24 +0900

    주석 입력완료

--- ConsoleApp1/GameManager.cs
+++ ConsoleApp1/GameManager.cs
@@ -8,39 +8,47 @@ namespace Las_Vegas
 {
     public class Dice
     {
-        private int playerNum;
+        /*
+         주사위 클래스
+         - 주사위와 관련된 데이터들을 관리하는 클래스이다.
+           주사위를 굴리고 고르고하는 등 주사위의 데이터를 해당 클래스에서 전부 관리한다.
+         */
+        private int playerCount;
         private int color;
         private int colerDiceCount;
         private int whiteDiceCount;
         private int[] arrColerDice = new int[6];
         private int[] arrWhiteDice = new int[6];
-       //private ArrayList arrColerDice = new ArrayList();
-        //private ArrayList arrWhiteDice = new ArrayList();
 
-        Random rand = new Random();
+        Random rand = new Random();//무작위 변수생성을 위한 선언
 
-        public Dice(int playerNum, int color)//생성자
+        public Dice(int playerCount, int color)//생성자
         {
-            Debug.Assert(playerNum > 0 && playerNum < 6,"Player number is Wrong");
-            this.color = color; //색을 클래스를 생성할 때 지정해 주자
-            this.playerNum = playerNum;
-            if (this.playerNum <= 4)
+            /*
+             이와같이 플레이어 수가 정해지면 그에따라 주사위 분배가 달라진다. 이는 원작 라스베가스를 따른다.
+             
+             */
+            Debug.Assert(playerCount > 0 && playerCount < 6,"Player number is Wrong");
+            this.color = color; //색을 클래스를 생성할 때 지정해 주자. 순서대로 빨(0), 초(1), 파(2), 검(3), 흰(4) 순서임 R, G, BU, BL, W, 
+            this.playerCount = playerCount; //플레이어 숫자에 따라 주사위 분배가 달라진다.
+            if (this.playerCount <= 4)
             {
                 this.colerDiceCount = 8;
                 this.whiteDiceCount = 2;
             }
-            else if (this.playerNum == 5)
+            else if (this.playerCount == 5)
             {
                 this.colerDiceCount = 8;
             }
         }
 
-        public int readWhiteDices()//흰색 주사위 갯수
-        {
-            return this.whiteDiceCount;
-        }
         public void rollDice()//주사위 굴리기(내가 가진 주사위 갯수만큼)
         {
+            /*
+             이 메소드를 실행하면 해당 클래스가 가지고있는 주사위 숫자만큼(칼라, 흰색 따로) 주사위를 굴려
+             arrColerDice, arrWhiteDice에 주사위를 넣어준다. 이때 배열의 위치는 주사위의 눈의 값이고 해당 index에 있는 정수값은 주사위의 갯수를 의미한다.
+             ex) arrWhiteDice = {0,0,1,0,0,1} => 3주사위 한개, 6주사위 1개
+             */
             int temp;
             Array.Clear(arrColerDice, 0, 6);
             Array.Clear(arrWhiteDice, 0, 6);
@@ -61,6 +69,11 @@ namespace Las_Vegas
             
         }
 
+        /*
+        아래 두 메소드는 고른 숫자의 주사위를 전체 가지고있는 주사위 숫자에서 빼주고 그 숫자만큼 반환하는 역할을 한다.
+        직접적으로 이용하는 일은 없고 주사위 클래스를 Board에 인자로 넘겨서 자동으로 연산을 이루어지도록 한다.
+        Board클래스의 placeDice메소드를 참고하라
+         */
         public int pickColorDice(int diceNumber)//고른 주사위 갯수만큼 주사위를 뺴고 해당 갯수를 반환
         {
             //Debug.Assert(!this.arrColerDice.Contains(diceNumber)|| !this.arrWhiteDice.Contains(diceNumber), "Player don't have that number dice");
@@ -81,11 +94,19 @@ namespace Las_Vegas
            
         }
 
+        /*
+         내 클래스의 색을 반환하는 메소드이다.
+         */
         public int getColor()
         {
             return this.color;
         }
 
+        /*
+         이는 GameManagerTest를 통해서 디버깅을 하기위한 메소드이다.
+         게임을 플레이할 수 있도록 콘솔창에 정보들을 출력해주는 메소드이므로
+         게임로직과는 크게 상관이없다.
+         */
         public void printDices()
         {
             Console.Write("Dice      ");
@@ -114,7 +135,10 @@ namespace Las_Vegas
 
     }
 
-
+    /*
+    보드와 관련된 클래스. 각 카지노에 어떤색의 주사위가 얼만큼 있는지, 얼마만큼의 돈이 있는지
+    관리하는 클래스이다.
+     */
     public class Board
     {
         private Random rand = new Random();
@@ -123,6 +147,10 @@ namespace Las_Vegas
         private int[,] casinoDice = new int[6,5];
         //private ArrayList[] casinoDice = new ArrayList[6];
 
+        /*
+         MoneyCards에서 카드를 섞은다음 무작위로 초기화된 카드 리스트를 받아 적절히 카지노에 분배한다.
+         이때 분배하는 방법은 원작 라스베가스의 룰을 따른다.
+        */
         public Board(List<List<int>> cards)
         {
             
@@ -221,6 +249,12 @@ namespace Las_Vegas
         }
 
     }
+
+    /*
+     * 카드들을 섞어주는 클래스. 
+     * 보드를 초기화해주는데 사용하고 게임 도중에는 크게 사용되는 일이 없는 클래스이다.
+     * 카드를 섞는 로직은 사용 언어마다, 툴 마다 다르므로 이러한 클래스와 메소드가 필요한정도로 넘어가면 될 듯 하다.
+     */
     class MoneyCards
     {
         private Random rand = new Random();
